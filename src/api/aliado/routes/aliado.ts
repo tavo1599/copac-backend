db0b022cb4cb51b818f861d8adb97031298a9bd4
tavo1599 <gustavo08_99@hotmail.com>
2025-10-06 21:29:27 -0500

    first commit

---
+++ src/api/aliado/routes/aliado.ts
@@ -0,0 +1,7 @@
+/**
+ * aliado router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::aliado.aliado');
---
+++ src/api/aliado/services/aliado.ts
@@ -0,0 +1,7 @@
+/**
+ * aliado service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::aliado.aliado');
